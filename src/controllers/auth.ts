06a2dc7e39cf89fe2101989c9e128b77aea0e83e
CalebAkomaye <calebakomaye@outlook.com>
2024-12-26 13:22:42 +0100

    📦 NEW: login route up and running

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -32,3 +32,42 @@ export const register = async (req: express.Request, res: express.Response) => {
     res.sendStatus(400);
   }
 };
+
+export const login = async (req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).json({ msg: 'Incorrect password or email' });
+      return;
+    }
+
+    const user = await getUserByEmail(email).select(
+      '+authentication.salt +authentication.password'
+    );
+    if (!user) {
+      res.status(400).json({
+        msg: 'credentials not registered, please signup to create an account',
+      });
+      return;
+    }
+
+    const tempHash = authenticate(user.authentication?.salt || '', password);
+    if (user.authentication?.password !== tempHash) {
+      res.status(403).json({ msg: 'Incorrect password' });
+      return;
+    }
+
+    const salt = randBytes();
+    user.authentication.sessionToken = authenticate(salt, user._id.toString());
+
+    await user.save();
+    res.cookie('mort', user.authentication.sessionToken, {
+      domain: 'localhost',
+      path: '/',
+    });
+    res.status(200).json({ msg: 'Login successful', user });
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
